60d30a491713e22901e2496edd2fb593214d128b
0xharshitha <harshithabathula1817@gmail.com>
2024-03-05 21:00:12 +0530

    fixed entry service span

--- entry/src/main/java/com/microcm/entry/models/Span.java
+++ entry/src/main/java/com/microcm/entry/models/Span.java
@@ -4,42 +4,18 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
+@Table(name = "spans")
 public class Span {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long span_id;
-
-    @ManyToOne
-    @JoinColumn(name = "transaction_id")
-    private Transaction transaction;
-
-    private Long response_time;
-
-    public Long getSpan_id() {
-        return span_id;
-    }
-
-    public Transaction getTransaction() {
-        return transaction;
-    }
-
-    public Long getResponse_time() {
-        return response_time;
-    }
-
-    public void setResponse_time(Long response_time) {
-        this.response_time = response_time;
-    }
-
-    public void setSpan_id(Long span_id) {
-        this.span_id = span_id;
-    }
-
-    public void setTransaction(Transaction transaction) {
-        this.transaction = transaction;
-    }
+    private Long spanId;
+    private Long responseTime;
+    private Long transactionId;
 }
