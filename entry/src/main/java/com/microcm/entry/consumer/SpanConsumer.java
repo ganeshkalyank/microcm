c09e2c741ec2225fd65974c1e9a2fc164f392801
jahnav-sannapureddy <jahnavsannapureddy@gmail.com>
2024-03-25 02:42:00 +0530

    rabbitMQ check ✅

--- entry/src/main/java/com/microcm/entry/consumer/SpanConsumer.java
+++ entry/src/main/java/com/microcm/entry/consumer/SpanConsumer.java
@@ -3,26 +3,28 @@ package com.microcm.entry.consumer;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.microcm.entry.models.Span;
 import com.microcm.entry.repositories.SpanRepository;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Component
 @Slf4j
+@RequiredArgsConstructor
 public class SpanConsumer {
     private final SpanRepository spanRepository;
+    private final ObjectMapper objectMapper;
 
-    public SpanConsumer(SpanRepository spanRepository) {
-        this.spanRepository = spanRepository;
-    }
 
     @RabbitListener(queues ="transmission-queue")
-    public void consumeSpan(String data){
-        // Span span = new Span();
-        // span.setResponseTime(spanRequest.getResponseTime());
-        // span.setTransactionId(spanRequest.getTransactionId());
-        // spanRepository.save(span);
+    public void consumeSpan(String data) throws Exception{
+        SpanRequest spanRequest = objectMapper.readValue(data, SpanRequest.class);
+        Span span = new Span();
+        span.setResponseTime(spanRequest.getResponseTime());
+        span.setTransactionId(spanRequest.getTransactionId());
+        spanRepository.save(span);
         log.info("Span recieved: {}", data);
     }
 }
