6fd45b36e4f477b47dce391a863e56c459124089
jahnav-sannapureddy <jahnavsannapureddy@gmail.com>
2024-04-12 22:20:08 +0530

    transaction id update

--- product-service/src/main/java/com/microcm/product/monitoring/MonitoringAspect.java
+++ product-service/src/main/java/com/microcm/product/monitoring/MonitoringAspect.java
@@ -2,6 +2,11 @@ package com.microcm.product.monitoring;
 
 import com.microcm.product.rabbitmq.RabbitMQConfig;
 
+import jakarta.servlet.http.HttpServletRequest;
+
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -33,30 +38,39 @@ public class MonitoringAspect {
         long endTime = System.currentTimeMillis();
         long executionTime = endTime - startTime;
 
-        String uri = "http://localhost:8080/transactions";
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+        String transactionId = request.getHeader("Transaction-ID");
+        String parentService = request.getHeader("Parent-Service");
+        if (transactionId == null) {
+            log.warn("Transaction-ID header not found in the request");
+            String uri = "http://localhost:8080/transactions";
+            RestTemplate restTemplate = new RestTemplate();
+            TransactionRequest transactionRequest = new TransactionRequest();
+            transactionRequest.setRequestedService("product");
+            
+            ResponseEntity<TransactionResponse> response = restTemplate.postForEntity(uri, transactionRequest , TransactionResponse.class);
+    
+            TransactionResponse transactionResponse = response.getBody(); 
+            Transaction transaction = transactionResponse.getData().get(0);
+    
+            if(response.getStatusCode() == HttpStatus.OK){
+                log.info("Successfully created the transaction ID");
+                log.info("Transaction ID: {}", transaction.getTransactionId());
+            }
 
-        RestTemplate restTemplate = new RestTemplate();
-
-        TransactionRequest transactionRequest = new TransactionRequest();
-        transactionRequest.setRequestedService("product");
-        
-        ResponseEntity<TransactionResponse> response = restTemplate.postForEntity(uri, transactionRequest , TransactionResponse.class);
-
-        TransactionResponse transactionResponse = response.getBody(); 
-        Transaction transaction = transactionResponse.getData().get(0);
-
-        if(response.getStatusCode() == HttpStatus.OK){
-            log.info("Successfully created the transaction ID");
-            log.info("Transaction ID: {}", transaction.getTransactionId());
+            transactionId = transaction.getTransactionId().toString();
+            parentService = "null";
+    
+        } else {
+            log.info("Transaction-ID header found: {}", transactionId);
         }
-
 
         SpanRequest spanRequest = new SpanRequest();
         spanRequest.setResponseTime(executionTime);
-        spanRequest.setTransactionId(transaction.getTransactionId());
+        spanRequest.setTransactionId(Long.parseLong(transactionId));
         spanRequest.setInvocationDateTime(LocalDateTime.now());
-        spanRequest.setParentService("parent service");
-        spanRequest.setRequestedService(transaction.getRequestedService());
+        spanRequest.setParentService(parentService);
+        spanRequest.setRequestedService("product");
 
         rabbitTemplate.convertAndSend(RabbitMQConfig.EXCHANGE, "", spanRequest);
 
@@ -69,7 +83,6 @@ public class MonitoringAspect {
         //     Span span = spanResponse.getData().get(0);
         //     log.info("Span ID: {}", span.getSpanId()); 
         // }
-
 
         log.info("{} executed in {} ms", joinPoint.getSignature(), executionTime);
         return result;
